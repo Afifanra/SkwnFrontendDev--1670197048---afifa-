608eccb07f8a26413f8ba1f99c25b64deab13ba7
Afifa Nur Aini <121655977+Afifanra@users.noreply.github.com>
2023-01-19 15:01:36 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,17 @@
+
+document.addEventListener('DOMContentLoaded', function () {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems);
+
+    document.querySelectorAll(".sidenav a, .topnav a").forEach(elm => {
+        elm.addEventListener("click", evt => {
+            let sideNav = document.querySelector(".sidenav");
+            M.Sidenav.getInstance(sideNav).close();
+            page = evt.target.getAttribute("href").substr(1);
+            loadPage(page);
+        })
+    })
+    var page = window.location.hash.substr(1);
+    if (page === "" || page === "!") page = "teams";
+    loadPage(page);
+});
